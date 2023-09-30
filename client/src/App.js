72c6cb2ccf670e253c527ba34c41d5148ebf0a9d
juhaj77 <juhaj358@gmail.com>
2023-10-01 01:57:45 +0300

    CSS refactoring

--- client/src/App.js
+++ client/src/App.js
@@ -8,24 +8,24 @@ const Tabs = ({onClick,show}) => {
   switch(show) {
     case 'ALERTS': return (
       <div style={{display:'inline-flex'}}>
-        <div style={{paddingLeft:'2em',paddingRight:'2em',background:'#303641',color:'white'}}>ALERTS</div>
-        <div onClick={() => onClick('APPID')} style={{borderLeft:"solid 1px #303641",paddingLeft:'2em',paddingRight:'2em',background:'#595c62',color:'white'}}>APPID</div>
-        <div onClick={() => onClick('SEARCH')} style={{borderLeft:"solid 1px #303641", paddingLeft:'2em',paddingRight:'2em',background:'#595c62',color:'white'}}>SEARCH</div>
+        <div className="active">ALERTS</div>
+        <div className="passive" onClick={() => onClick('APPID')}>APPID</div>
+        <div className="passive" onClick={() => onClick('SEARCH')}>SEARCH</div>
       </div>)
    case 'APPID': 
     return (
       <div style={{display:'inline-flex'}}>
-        <div onClick={() => onClick('ALERTS')} style={{paddingLeft:'2em',paddingRight:'2em',background:'#595c62',color:'white'}}>ALERTS</div>
-        <div style={{paddingLeft:'2em',paddingRight:'2em',background:'#303641',color:'white'}}>APPID</div>
-        <div onClick={() => onClick('SEARCH')} style={{borderLeft:"solid 1px #303641",paddingLeft:'2em',paddingRight:'2em',background:'#595c62',color:'white'}}>SEARCH</div>
+        <div className="passive" onClick={() => onClick('ALERTS')}>ALERTS</div>
+        <div className="active">APPID</div>
+        <div className="passive" onClick={() => onClick('SEARCH')}>SEARCH</div>
       </div>)
   
   case 'SEARCH': 
     return (
       <div style={{display:'inline-flex'}}>
-        <div onClick={() => onClick('ALERTS')} style={{paddingLeft:'2em',paddingRight:'2em',background:'#595c62',color:'white'}}>ALERTS</div>
-        <div onClick={() => onClick('APPID')} style={{borderLeft:"solid 1px #303641",paddingLeft:'2em',paddingRight:'2em',background:'#595c62',color:'white'}}>APPID</div>
-        <div style={{paddingLeft:'2em',paddingRight:'2em',background:'#303641',color:'white'}}>SEARCH</div>
+        <div className="passive" onClick={() => onClick('ALERTS')}>ALERTS</div>
+        <div className="passive" onClick={() => onClick('APPID')}>APPID</div>
+        <div className="active">SEARCH</div>
       </div>)
   }
 }
