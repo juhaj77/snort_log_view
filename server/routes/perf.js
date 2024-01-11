74f3990a2c85c36305cab32307e8b861d00d7f07
juhaj77 <juhaj358@gmail.com>
2024-01-11 22:28:07 +0200

    more read time

--- server/routes/perf.js
+++ server/routes/perf.js
@@ -15,7 +15,7 @@ router.get('/',async function(req, res, next) {
         tail.on('line', data => {
             array.push(data)
         })
-        await sleep(index*10) // time for reading
+        await sleep(index*20) // time for reading
         response = array
         index++
     } while (array[0].split(',')[0] != '#timestamp')
